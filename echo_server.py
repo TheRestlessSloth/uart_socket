b3013520a109a2a3de0295184c2d241544888591
Timur Abkerimov <58333949+TheRestlessSloth@users.noreply.github.com>
2022-07-20 17:09:04 +0300

    upd socket

--- echo_server.py
+++ echo_server.py
@@ -3,24 +3,24 @@ from time import sleep
 
 #=-----------------config----------------
 #socket
-host = "192.168.0.104"
+host = "10.32.31.13"
 port = 65432
 
 d_del = 0.1
 
-
 def rx_md(conn):
 	data = 0
 	while True:
-    	data += 1 
-		if not data:
+		if data == 100:
 			break
-		d = str(data)
-		conn.sendall(data.encode())
+		sleep(d_del)
+		data += 1
+		d = str(data) 
+		conn.sendall(d.encode())
 
 def tx_md(conn):
 	while True:
-    	data = conn.recv(1024).decode()
+		data = conn.recv(1024)
 		if data == -666:
 			break
 		print(data)
@@ -34,13 +34,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		print(f"Connected by {addr}")
 		
 		#main_program
-		conn.sendall("Receive/Transmit/exit? (1, 2, 3)".encode())
-		md = conn.recv(1024).decode()
-		#modes
-		if md == 1:
-			rx_md(conn)
-		elif md == 2:
-			tx_md(conn)
-		else
-
+		while True:
+			conn.sendall("Receive/Transmit/exit? (1, 2, 3) ".encode())
+			md = conn.recv(1024).decode('utf-8', 'ignore').strip()
+			print(md)
+			#modes
+			if md == "1":
+				rx_md(conn)
+			elif md == "2":
+				tx_md(conn)
+			elif md == "3":
+				break
+			else:
+				continue
 		print("Disconnected")	
