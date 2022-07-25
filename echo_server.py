dbbd71e119c32b64e960ca607499c4d37d8e5a70
Mohi-mo <61887500+Mohi-mo@users.noreply.github.com>
2022-07-25 14:22:25 +0300

    Add files via upload

--- echo_server.py
+++ echo_server.py
@@ -4,40 +4,31 @@ from time import sleep
 
 #=-----------------config----------------
 #socket
-host = "192.168.0.104"
+host = "192.169.3.99"
 port = 65432
 
-#uart
-ttyn = "/dev/ttyS0"
-baudrt = 9600
-
 d_del = 0.1
-ser = serial.Serial(ttyn, baudrt)
+
+ser = serial.Serial("/dev/serial0", 9600)
 
 def uart_rx():
-	#data collection from uart
-    r_data = ser.read()
-    sleep(0.03)
-    d_left = ser.inWaiting()
-    r_data += ser.read(d_left)
-    return str(r_data)
+	data = ser.read()
+	return data
 
 def rx_md(conn):
 	while True:
-    	data = uart_rx()
-		if not data:
-			break
-		conn.sendall(data.encode())
+		data = uart_rx()
+		#if not data:
+			#break
+		sleep(d_del) 
+		conn.sendall(data)
 
 def tx_md(conn):
 	while True:
-    	data = conn.recv(1024).decode()
-		if data == -666:
-			break
-		ser.write(data)
-
-def main_p(conn):
-	
+		data = conn.recv(1024)
+		#if data == -666:
+			#break
+		ser.write(str(data).encode("ascii"))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.bind((host,port))
@@ -48,13 +39,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		print(f"Connected by {addr}")
 		
 		#main_program
-		conn.sendall("Receive/Transmit/exit? (1, 2, 3)".encode())
-		md = conn.recv(1024).decode()
-		#modes
-		if md == 1:
-			rx_md(conn)
-		elif md == 2:
-			tx_md(conn)
-		else
-
-		print("Disconnected")	
+		while True:
+			conn.sendall("Receive/Transmit/exit? (1, 2, 3) ".encode())
+			md = conn.recv(1024).decode('ascii', 'ignore').strip()
+			print(md)
+			#modes
+			if md == "1":
+				rx_md(conn)
+			elif md == "2":
+				tx_md(conn)
+			elif md == "3":
+				break
+			else:
+				continue
+		print("Disconnected")
