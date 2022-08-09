d6946eed734dcab14fedc0e579ecff35be38dcd5
Timur Abkerimov <58333949+TheRestlessSloth@users.noreply.github.com>
2022-08-09 08:20:10 -0700

    upd server

--- uartsock-bridge-v0.3.1.py
+++ uartsock-bridge-v0.3.1.py
@@ -1,23 +1,24 @@
 import socket
 import serial
-from multiprocessing import Process
 import os
+
 from time import sleep
 from PIL import Image, ImageFile
+from multiprocessing import Process
 
 
 # classes and methods
 class Uart:
     # UART CLASS
     def __init__(self, ser_port, baud):
-        self.ser_port = ser_port
-        self.baud = baud
-        self.ser = serial.Serial(self.ser_port, self.baud)
+        self.SER_PORT = ser_port
+        self.BAUD = baud
+        self.ser = serial.Serial(self.SER_PORT, self.BAUD)
 
-    def uart_rx(self):
+    def rx(self):
         return self.ser.readline()
 
-    def uart_tx(self, data):
+    def tx(self, data):
         self.ser.write(data)
         self.ser.write("\r\n".encode("ascii"))
 
@@ -25,91 +26,22 @@ class Uart:
 class Socket:
     # SOCKET CLASS
     def __init__(self, host, port):
-        self.__host = host
-        self.__port = port
+        self.HOST = host
+        self.PORT = port
         # connect
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.s.bind((self.__host, self.__port))
+        self.s.bind((self.HOST, self.PORT))
         self.s.listen()
+
         self.conn, self.addr = self.s.accept()
 
-    def sock_rx(self):
+    def rx(self):
         return self.conn.recv(1024).decode("ascii", "ignore").strip()
 
-    def sock_tx(self, data):
+    def tx(self, data):
         self.conn.sendall(data)
         self.conn.sendall("\r\n".encode("ascii"))
-
-
-class MainProg:
-    # main program
-    def __init__(self, host, port, ser_port, baud):
-        self.__host = host
-        self.__port = port
-        self.__ser_port = ser_port
-        self.__baud = baud
-
-    def connect(self):
-        self.server = Socket(self.__host, self.__port)
-        self.uart = Uart(self.__ser_port, self.__baud)
-
-    def rx_md(self):
-        rx_data = self.uart.uart_rx().decode("ascii").strip()
-        if rx_data == "stop":
-            print("stopped from uart")
-            return False
-        else:
-            self.server.sock_tx(rx_data.encode("ascii"))
-            return True
-
-    def tx_md(self):
-        tx_data = self.server.sock_rx()
-        if not tx_data:
-            return True
-        if tx_data == "stop":
-            print("stopped from socket")
-            return False
-        else:
-            self.uart.uart_tx(tx_data.encode("ascii"))
-            return True
-
-    def old_lp(self):
-        while True:
-            self.server.conn.send("Receive/Transmit/exit? (1, 2, 3) ".encode("ascii"))
-            md = self.server.sock_rx()
-            if not md:
-                continue
-            print("Mode = " + md)
-            # modes
-            if md == "1":
-                while self.rx_md():
-                    pass
-            elif md == "2":
-                while self.tx_md():
-                    pass
-            elif md == "3":
-                break
-            else:
-                continue
-
-
-class NewProg(MainProg):
-    def new_rx(self):
-        while self.rx_md():
-            pass
-
-    def new_tx(self):
-        while self.tx_md():
-            pass
-
-    def main_lp(self):
-        p1 = Process(target=self.new_rx)
-        p2 = Process(target=self.new_tx)
-        p1.start()
-        p2.start()
-        p1.join()
-        p2.join()
 
 
 class PictureModule:
@@ -127,7 +59,7 @@ class PictureModule:
             if not s:
                 break
             p.feed(s)
-            main_prog.server.sock_tx(s)
+            main_prog.sock.tx(s)
         p.close()
 
     def send_pic_uart(self, path, main_prog):
@@ -138,8 +70,83 @@ class PictureModule:
             if not s:
                 break
             p.feed(s)
-            main_prog.uart.uart_tx(s)
+            main_prog.uart.tx(s)
         p.close()
+
+
+class MainProg:
+    # main program
+    def __init__(self, host, port, ser_port, baud):
+        self.HOST = host
+        self.PORT = port
+        self.SER_PORT = ser_port
+        self.BAUD = baud
+
+    def connect(self):
+        self.sock = Socket(self.HOST, self.PORT)
+        self.uart = Uart(self.SER_PORT, self.BAUD)
+
+    def rx_md(self):
+        rx_data = self.uart.rx().decode("ascii").strip()
+        if rx_data == "stop":
+            print("Stopped from UART")
+            return -1
+        if rx_data == "send nudes":
+            print("sending nudes fr Uart")
+            return 2
+        else:
+            self.sock.tx(rx_data.encode("ascii"))
+            return 1
+
+    def tx_md(self):
+        tx_data = self.sock.rx()
+        if not tx_data:
+            return 1
+        if tx_data == "stop":
+            print("Stopped from Socket")
+            return -1
+        if tx_data == "send nudes":
+            print("sending nudes fr Socket")
+            return 2
+        else:
+            self.uart.tx(tx_data.encode("ascii"))
+            return 1
+
+
+class MultiThread(MainProg):
+
+    def pm_init(self):
+        self.pm = PictureModule()
+    
+    def mt_rx(self):
+        while True:
+            ret = self.rx_md()
+            if ret == -1:
+                break
+            elif ret == 2:
+                self.pm.send_pic_uart("./files/12.jpg",self)
+            else:
+                pass
+
+    def mt_tx(self):
+        while True:
+            ret = self.tx_md()
+            if ret == -1:
+                break
+            elif ret == 2:
+                self.pm.send_pic_socket("./files/12.jpg",self)
+            else:
+                pass
+                
+                          
+    def main_lp(self):
+        self.rx_p = Process(target = self.mt_rx)
+        self.tx_p = Process(target = self.mt_tx)
+        self.rx_p.start()
+        self.tx_p.start()
+        self.rx_p.join()
+        self.tx_p.join()
+  
 
 
 # -----------------config----------------
@@ -148,17 +155,13 @@ PORT = 65432
 SER_PORT = "/dev/ttyUSB0"
 BAUD = 9600
 
-pm = PictureModule()
-
 # program
 if __name__ == "__main__":
-    mp = NewProg(HOST, PORT, SER_PORT, BAUD)
+    mp = MultiThread(HOST, PORT, SER_PORT, BAUD)
     mp.connect()
-    with mp.server.conn:
-        print(f"Connected by {mp.server.addr}")
-        pm.send_pic_uart("./files/12.jpg",mp)
-        # main_program
+    mp.pm_init()
+    with mp.sock.conn:
+        print(f"Connected by {mp.sock.addr}")
         mp.main_lp()
-        # mp.old_lp()
-    mp.server.conn.close()
+    mp.sock.conn.close()
     print("Disconnected")
