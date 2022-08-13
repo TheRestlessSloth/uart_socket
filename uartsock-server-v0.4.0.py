27e8e19b81a792525500bcf23ea6c27f89a608df
Timur Abkerimov <58333949+TheRestlessSloth@users.noreply.github.com>
2022-08-13 08:33:32 +0300

    main upd

--- uartsock-server-v0.4.0.py
+++ uartsock-server-v0.4.0.py
@@ -1,16 +1,19 @@
 import os
+import sys
+
+sys.path.insert(1, "./modules")
+
 from uart_module import *
 from sock_module import *
 from pic_module import *
-
 from time import sleep
 from multiprocessing import Process
 
 
 # -----------------config----------------
-HOST = "10.32.31.17"  # "192.169.3.98"
+HOST = "192.168.0.103"  # "192.169.3.98"
 PORT = 65432
-SER_PORT = "/dev/ttyUSB1"
+SER_PORT = "COM1"
 BAUD = 9600
 commands = {'stop':-1,'send':2,'sendfft':3}
 im_src = "./files/test.png"
@@ -74,8 +77,7 @@ class MultiThread(MainProg):
         self.sut_p.join()
 
 
-# program
-if __name__ == "__main__":
+def main():
     mp = MultiThread(HOST, PORT, SER_PORT, BAUD)
     mp.connect()
     with mp.sock.conn:
@@ -83,3 +85,7 @@ if __name__ == "__main__":
         mp.main_lp()
     mp.sock.conn.close()
     print("Disconnected")
+
+# program
+if __name__ == "__main__":
+    main()
