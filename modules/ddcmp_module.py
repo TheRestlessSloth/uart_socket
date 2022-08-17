f5f9a208037551356cff6ad86ebd95c70927bd2c
rolets <58333949+TheRestlessSloth@users.noreply.github.com>
2022-08-17 17:36:32 +0300

    upd

--- modules/ddcmp_module.py
+++ modules/ddcmp_module.py
@@ -2,7 +2,102 @@ from time import sleep
 from uart_module import *
 import threading
 
-TCNT_OVF = False
+com = "COM4"
+baud = 9600
+encoding = "utf-8"
+
+uart = Uart(com,9600,encoding)
+
+class DDCMP:
+    def __init__(self,timeout):
+        self.timeout = 4
+        self.states = ("HALTED",
+                       "ISTART",
+                       "ASTART",
+                       "RUNNING",
+                       "INVALID")
+        self.cur_state = self.states[0]
+    
+    def state_mes(self):
+        ret = "State: " + self.cur_state
+        print(ret)
+        uart.tx(ret)
+
+    def timer_pr(self):
+        self.TCNT_OVF = False
+        for counter in range(0,self.timeout*10):
+            sleep(0.1)
+        self.TCNT_OVF = True
+        return
+
+    def uart_tx(self):
+        while True:
+            string = ">>> " + input()
+            print(string)
+            uart.tx(string)
+
+    def uart_rx(self):
+        while True:
+            data = uart.rx()
+            if data is not None: 
+                print("<<< ",data)
+
+    def handshake(self,timer_p):
+        self.cur_state = self.states[1] 
+        self.state_mes()
+        
+        timer_p.start()
+
+        while not self.TCNT_OVF:
+            mes = uart.rx()
+            if mes == "ok":
+                timer_p.counter = self.timeout
+                return True 
+        return False
+
+    def mk_connection(self,tx_p,rx_p,timer_p):
+        if self.cur_state is not self.states[3]:
+            if self.handshake(timer_p):
+                self.cur_state = self.states[3]
+                self.state_mes()
+                tx_p.start()
+                rx_p.start()
+            else:
+                print("timeout. making another handshake")
+                return False
+        else:
+            return False
+
+
+def main():
+    ddcmp = DDCMP(4)
+    timer_p = threading.Thread(target=ddcmp.timer_pr)
+    timer_p.__reduce__
+    tx_p = threading.Thread(target=ddcmp.uart_tx)
+    rx_p = threading.Thread(target=ddcmp.uart_rx)
+    while not ddcmp.mk_connection(tx_p,rx_p,timer_p):
+        pass
+
+if __name__ == "__main__":
+    main()
+
+'''
+while True:
+    Timer_process()
+    DC_input_process()
+    DC_output_process()
+
+    if ip_ready:
+        onIncomingPacket()
+    
+    if((state == DDCMP.states("HALTED")) && ((ih_Cnt>0) || (isStartup && autostart))):
+        if isStartup:
+            isStartup = False
+        state = DDCMP.states("ISTART")
+        CTRLSEND(ptype_str, 0, 0, True)
+    elif state == DDCMP.states("RUNNING"):
+        protocol_perf()
+'''
 
 '''
 class DDCMP:
@@ -53,82 +148,4 @@ class DDCMP:
         self.SELECT = True
         self.PTYPE = 49
         uart.tx(data)
-'''
-
-uart = Uart("COM6",9600)
-
-def OVF():
-    TCNT_OVF = True
-
-def timer_pr():
-    a = 0
-    TCNT_OVF = False
-    while True:
-        TCNT_OVF = False
-        if a >= 5:
-            a = 0
-            OVF()
-        a += 0.1
-        sleep(0.1)
-
-def uart_tx():
-    while True:
-        string = ">>> " + input()
-        print(string)
-        uart.tx(string.encode("ascii"))
-
-def uart_rx():
-    while True:
-        print("\n<<< ",uart.rx())
-
-def handshake(timer_p):
-    print("State: START")
-    uart.tx("State: START".encode("ascii"))
-    timer_p.start()
-    a = uart.rx()
-    if TCNT_OVF:
-        return False
-    else:
-        if  == "ok":
-            return True 
-
-def mk_connection(tx_p,rx_p,timer):
-    if handshake(timer):
-        uart.tx("State: RUNNING".encode("ascii"))
-        print("State: RUNNING")
-        tx_p.start()
-        rx_p.start()
-
-    else:
-        print("try another handshake")
-        
-        
-    
-def main():
-    timer_p = threading.Thread(target=timer_pr)
-    tx_p = threading.Thread(target=uart_tx)
-    rx_p = threading.Thread(target=uart_rx)
-    mk_connection(tx_p,rx_p)
-    
-
-
-if __name__ == "__main__":
-    main()
-
-'''
-while True:
-    Timer_process()
-    DC_input_process()
-    DC_output_process()
-
-    if ip_ready:
-        onIncomingPacket()
-    
-    if((state == DDCMP.states("HALTED")) && ((ih_Cnt>0) || (isStartup && autostart))):
-        if isStartup:
-            isStartup = False
-        state = DDCMP.states("ISTART")
-        CTRLSEND(ptype_str, 0, 0, True)
-    elif state == DDCMP.states("RUNNING"):
-        protocol_perf()
 '''
--- modules/uart_module.py
+++ modules/uart_module.py
@@ -1,19 +1,19 @@
+import encodings
 import serial
 
 # classes and methods
 class Uart:
     # UART CLASS
-    def __init__(self, ser_port, baud):
+    def __init__(self, ser_port, baud, encoding = "ascii"):
         self.SER_PORT = ser_port
         self.BAUD = baud
         self.ser = serial.Serial(self.SER_PORT, self.BAUD)
+        self.encoding = encoding
 
     def rx(self):
         if self.ser.inWaiting():
-            return self.ser.readline().decode("ascii").strip()
-        else:
-            return "none"
+            return self.ser.readline().decode(self.encoding).strip()
 
     def tx(self, data):
-        self.ser.write(data)
-        self.ser.write("\r\n".encode("ascii"))
+        self.ser.write(data.encode(self.encoding))
+        self.ser.write("\r\n".encode(self.encoding))
